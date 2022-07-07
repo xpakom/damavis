430ab0f830d94d62e9c8ec66ef2b2535a40c02b1
Francisco MG <cvfranciscomg@gmail.com>
2022-07-07 14:34:45 +0200

    Menu, delete dev prints

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 from snake import numberOfAvailableDifferentPaths
 
 if __name__ == "__main__":
-    #n1 = numberOfAvailableDifferentPaths([4, 3], [[2,2],[3,2],[3,1],[3,0],[2,0],[1,0],[0,0]], 3)
-    #n2 = numberOfAvailableDifferentPaths([2, 3], [[0,2],[0,1],[0,0],[1,0],[1,1],[1,2]], 10)
-    n3 = numberOfAvailableDifferentPaths([10, 10], [[5,5],[5,4],[4,4],[4,5]], 4)
-    #print ("Test 1: " + str(n1))
-    #print ("Test 2: " + str(n2))
-    print ("Test 3: " + str(n3))
+    option = -1
+    while(int(option) != 0):
+        print("SNAKE CALCULATOR by Francisco Munoz Gomez\n\n")
+        print("MENU (input the option number): \n")
+        print("1. Acceptance tests.\n")
+        print("2. Custom test.\n")
+        print("0. Exit")
+        option = input("\n")
+
+        if int(option) == 1:
+            print("\nTest 1:\n")
+            numberOfAvailableDifferentPaths([4, 3], [[2,2],[3,2],[3,1],[3,0],[2,0],[1,0],[0,0]], 3)
+            print("\nTest 2:\n")
+            numberOfAvailableDifferentPaths([2, 3], [[0,2],[0,1],[0,0],[1,0],[1,1],[1,2]], 10)
+            print("\nTest 3:\n")
+            numberOfAvailableDifferentPaths([10, 10], [[5,5],[5,4],[4,4],[4,5]], 4)
+            option = -1 
+        if int(option) == 2:
+            print("RESPECT THE INSTRUCTIONS, MAXIMUS AND MINIMUS FOR GETTING A CORRECT SOLUTION.")
+            print("IF ANY OF THE DATA IS OUT OF BOUNDS, THE CALCULUS WON'T DO AND YOU WILL HAVE TO ENTER ALL DATA AGAIN.")
+            board_h = 0
+            board_w = 0
+            board_w = int(input("\nEnter the width of the board (maximum 10):\n"))
+            board_h = int(input("\nEnter the heigth of the board (maximum 10):\n"))
+            board = []
+            board.append(board_w)
+            board.append(board_h)
+            
+            snakeSize = 0
+            snakeSize = int(input("""\nWrite the size of the snake (the size of the snake must be between 3 and 7): \n"""))
+            snake = []
+            count = 0
+            print("Introduce all the cells of the snake one by one, starting with the head:\n")
+            while count < snakeSize:
+                pos_x = int(input("Introduce cell " + str((count + 1)) + " x coodinate (must be between 0 and the width of the board\n"))
+                pos_y = int(input("Introduce cell " + str((count + 1)) + " y coodinate (must be between 0 and the heigth of the board\n"))
+                pos = []
+                pos.append(pos_x)
+                pos.append(pos_y)
+                snake.append(pos)
+                count = count + 1
+
+            depth = int(input("Enter the path depth (must be between 1 an 20): "))
+            numberOfAvailableDifferentPaths(board,snake,depth)
+            option = -1 
--- snake.py
+++ snake.py
@@ -1,65 +1,52 @@
-
+import sys
 paths = []
-COUNTER = 0
-
-def increment():
-    global COUNTER
-    COUNTER = COUNTER + 1
-
-def reset():
-    global COUNTER
-    COUNTER = 0
 
 def checkValidData(board, snake, depth):
-    if len(board) != 2 or 10 < board[0] or board[0] < 0 or 10 < board[1] or board[1] < 0:
-        return "The size of the board is out of bounds. Must be between 1 and 10."
+    correct = True
+    if len(board) != 2 or 10 <= board[0] or board[0] < 1 or 10 <= board[1] or board[1] < 1:
+        print("The size of the board is out of bounds. Must be between 1 and 10.")
+        correct = False
     if 3 <= len(snake) <= 7:
         for pos in snake:
             if len(pos) != 2:
-                return "The size of at least one of the coordinates of the snake is wrong."
-            if 0 > pos[0] or pos[0] > board[0] or 0 > pos[1] or pos[1] > board[1]:
-                return "At least one of the coordinates of the snake is out of bounds."
+                print("The size of at least one of the coordinates of the snake is wrong.")
+                correct = False
+            if 0 > pos[0] or pos[0] > board[0]-1 or 0 > pos[1] or pos[1] > board[1]-1:
+                print("At least one of the coordinates of the snake is out of bounds.")
+                correct = False
     else:
-        return "The snake is too short or too long. Must be between 3 and 7."
+        print("The snake is too short or too long. Must be between 3 and 7.")
+        correct = False
     if depth < 1 or depth > 20:
-        return "The depth of the path must be between 1 and 20."
+        print("The depth of the path must be between 1 and 20.")
+        correct = False
     
-    return "Checking succesfull. All data is OK."
+    if correct:
+        print("Checking succesfull. All data is OK.")
+    return correct
 
 def upPos (pos):
-    print ("Select up")
     u = []
     u.append(pos[0])
     u.append(pos[1] - 1)
-    print(u)
-    print('\n')
     return u
 
 def downPos (pos):
-    print ("Select down")
     d = []
     d.append(pos[0])
     d.append(pos[1] + 1)
-    print(d)
-    print('\n')
     return d
 
 def rightPos (pos):
-    print ("Select right")
     r = []
     r.append(pos[0] + 1)
     r.append(pos[1])
-    print(r)
-    print('\n')
     return r
 
 def leftPos (pos):
-    print ("Select left")
     l = []
     l.append(pos[0] - 1)
     l.append(pos[1])
-    print(l)
-    print('\n')
     return l
 
 def checkCollision(snake, nextPos):
@@ -93,11 +80,6 @@ def createPaths(board, snake, depth, actualPath):
             for pt in actualPath:
                 actualPathUp.append(pt)
             actualPathUp.append(u)
-            print ("Going up")
-            print(snakeUp)
-            print(depth - 1)
-            print(actualPathUp)
-            print('\n')
             createPaths(board, snakeUp, depth - 1, actualPathUp)
         if not checkCollision(snake,d) and not checkOutBounds(board,d):
             snakeDown = snakeNextPos(snake,d)
@@ -105,11 +87,6 @@ def createPaths(board, snake, depth, actualPath):
             for pt in actualPath:
                 actualPathDown.append(pt)
             actualPathDown.append(d)
-            print ("Going down")
-            print(snakeDown)
-            print(depth - 1)
-            print(actualPathDown)
-            print('\n')
             createPaths(board, snakeDown, depth - 1, actualPathDown)
         if not checkCollision(snake,r) and not checkOutBounds(board,r):
             snakeRight = snakeNextPos(snake,r)
@@ -117,11 +94,6 @@ def createPaths(board, snake, depth, actualPath):
             for pt in actualPath:
                 actualPathRight.append(pt)
             actualPathRight.append(r)
-            print ("Going right")
-            print(snakeRight)
-            print(depth - 1)
-            print(actualPathRight)
-            print('\n')
             createPaths(board, snakeRight, depth - 1, actualPathRight)
         if not checkCollision(snake,l) and not checkOutBounds(board,l):
             snakeLeft = snakeNextPos(snake,l)
@@ -129,27 +101,29 @@ def createPaths(board, snake, depth, actualPath):
             for pt in actualPath:
                 actualPathLeft.append(pt)
             actualPathLeft.append(l)
-            print ("Going left")
-            print(snakeLeft)
-            print(depth - 1)
-            print(actualPathLeft)
-            print('\n')
             createPaths(board, snakeLeft, depth - 1, actualPathLeft)
     else:
-        #print (actualPath)
-        print("+1")
-        print('\n')
-        increment()
         if (not actualPath in paths):
             paths.append(actualPath)
 
 
 def numberOfAvailableDifferentPaths(board, snake, depth):
-    checking =  checkValidData(board,snake,depth)
-    if checking != "Checking succesfull. All data is OK.":
-        return checking + "\nRevise the data and retry."
+    try:
+        checking =  checkValidData(board,snake,depth)
+        if not checking:
+            print("Revise the data and retry.")
+    except:
+        print("Incorrect data syntax. Please retry.")
     
-    reset()
-    createPaths(board, snake, depth, [])
-    pathsNumber = len(paths)
-    return pathsNumber
+    if checking:
+        createPaths(board, snake, depth, [])
+        pathsNumber = len(paths)
+        print("Number of possible paths: " + str(pathsNumber))
+        if pathsNumber > 0:
+            text = input("Do you want to see the list of possible paths? Press 'y' for yes or any key for no:  ")
+            if text == 'y' or text == 'Y':
+                for p in paths:
+                    print(p)
+
+    print('\n\n')
+    paths.clear()
