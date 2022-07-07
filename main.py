94c2223c9549057c951f1b13e6163493bc9edcde
Francisco MG <cvfranciscomg@gmail.com>
2022-07-07 11:22:04 +0200

    Bug fixing and logs

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import snake
+from snake import numberOfAvailableDifferentPaths
 
 if __name__ == "__main__":
-    print('Hello world')
+    #n1 = numberOfAvailableDifferentPaths([4, 3], [[2,2],[3,2],[3,1],[3,0],[2,0],[1,0],[0,0]], 3)
+    #n2 = numberOfAvailableDifferentPaths([2, 3], [[0,2],[0,1],[0,0],[1,0],[1,1],[1,2]], 10)
+    n3 = numberOfAvailableDifferentPaths([10, 10], [[5,5],[5,4],[4,4],[4,5]], 4)
+    #print ("Test 1: " + str(n1))
+    #print ("Test 2: " + str(n2))
+    print ("Test 3: " + str(n3))
--- snake.py
+++ snake.py
@@ -1,7 +1,16 @@
 
 paths = []
+COUNTER = 0
 
-def checkValidData(board:list, snake:list, depth:int):
+def increment():
+    global COUNTER
+    COUNTER = COUNTER + 1
+
+def reset():
+    global COUNTER
+    COUNTER = 0
+
+def checkValidData(board, snake, depth):
     if len(board) != 2 or 10 < board[0] or board[0] < 0 or 10 < board[1] or board[1] < 0:
         return "The size of the board is out of bounds. Must be between 1 and 10."
     if 3 <= len(snake) <= 7:
@@ -18,61 +27,129 @@ def checkValidData(board:list, snake:list, depth:int):
     return "Checking succesfull. All data is OK."
 
 def upPos (pos):
-    return [pos[0],pos[1] - 1]
+    print ("Select up")
+    u = []
+    u.append(pos[0])
+    u.append(pos[1] - 1)
+    print(u)
+    print('\n')
+    return u
 
 def downPos (pos):
-    return [pos[0],pos[1]+1]
+    print ("Select down")
+    d = []
+    d.append(pos[0])
+    d.append(pos[1] + 1)
+    print(d)
+    print('\n')
+    return d
 
 def rightPos (pos):
-    return [pos[0] + 1,pos[1]]
+    print ("Select right")
+    r = []
+    r.append(pos[0] + 1)
+    r.append(pos[1])
+    print(r)
+    print('\n')
+    return r
 
 def leftPos (pos):
-    return [pos[0] - 1,pos[1]]
+    print ("Select left")
+    l = []
+    l.append(pos[0] - 1)
+    l.append(pos[1])
+    print(l)
+    print('\n')
+    return l
 
-def checkCollision(snake:list, nextPos:list):
+def checkCollision(snake, nextPos):
     if nextPos in snake[:-1]:
         return True
     else:
         return False
 
-def checkOutBounds(board:list, nextPos:list):
+def checkOutBounds(board, nextPos):
     if nextPos[0] < board[0] and nextPos[1] < board[1] and nextPos [0] >= 0 and nextPos[1] >= 0:
         return False
     else:
         return True
 
-def snakeNextPos(snake:list, nextPos:list):
-    return [snake[-1]] + nextPos
+def snakeNextPos(snake, nextPos):
+    snakeNext = []
+    snakeNext.append(nextPos)
+    for ps in snake[:-1]:
+        snakeNext.append(ps)
+    return snakeNext
 
-def createPaths(board:list, snake:list, depth:int, actualPath:list):
+def createPaths(board, snake, depth, actualPath):
     if depth > 0:
-        if not checkCollision(board,snake,upPos(snake[0])) and not checkOutBounds(board,upPos(snake[0])):
-            snakeUp = snakeNextPos(snake,upPos(snake[0]))
-            actualPathUp = actualPath.append(upPos(snake[0]))
+        u = upPos(snake[0])
+        d = downPos(snake[0])
+        r = rightPos(snake[0])
+        l = leftPos(snake[0])
+        if not checkCollision(snake,u) and not checkOutBounds(board,u):
+            snakeUp = snakeNextPos(snake,u)
+            actualPathUp = []
+            for pt in actualPath:
+                actualPathUp.append(pt)
+            actualPathUp.append(u)
+            print ("Going up")
+            print(snakeUp)
+            print(depth - 1)
+            print(actualPathUp)
+            print('\n')
             createPaths(board, snakeUp, depth - 1, actualPathUp)
-        if not checkCollision(board,snake,downPos(snake[0])) and not checkOutBounds(board,upPos(snake[0])):
-            snakeDown = snakeNextPos(snake,downPos(snake[0]))
-            actualPathDown = actualPath.append(downPos(snake[0]))
+        if not checkCollision(snake,d) and not checkOutBounds(board,d):
+            snakeDown = snakeNextPos(snake,d)
+            actualPathDown = []
+            for pt in actualPath:
+                actualPathDown.append(pt)
+            actualPathDown.append(d)
+            print ("Going down")
+            print(snakeDown)
+            print(depth - 1)
+            print(actualPathDown)
+            print('\n')
             createPaths(board, snakeDown, depth - 1, actualPathDown)
-        if not checkCollision(board,snake,rightPos(snake[0])) and not checkOutBounds(board,upPos(snake[0])):
-            snakeRight = snakeNextPos(snake,rightPos(snake[0]))
-            actualPathRight = actualPath.append(rightPos(snake[0]))
+        if not checkCollision(snake,r) and not checkOutBounds(board,r):
+            snakeRight = snakeNextPos(snake,r)
+            actualPathRight = []
+            for pt in actualPath:
+                actualPathRight.append(pt)
+            actualPathRight.append(r)
+            print ("Going right")
+            print(snakeRight)
+            print(depth - 1)
+            print(actualPathRight)
+            print('\n')
             createPaths(board, snakeRight, depth - 1, actualPathRight)
-        if not checkCollision(board,snake,leftPos(snake[0])) and not checkOutBounds(board,upPos(snake[0])):
-            snakeLeft = snakeNextPos(snake,leftPos(snake[0]))
-            actualPathLeft = actualPath.append(leftPos(snake[0]))
+        if not checkCollision(snake,l) and not checkOutBounds(board,l):
+            snakeLeft = snakeNextPos(snake,l)
+            actualPathLeft = []
+            for pt in actualPath:
+                actualPathLeft.append(pt)
+            actualPathLeft.append(l)
+            print ("Going left")
+            print(snakeLeft)
+            print(depth - 1)
+            print(actualPathLeft)
+            print('\n')
             createPaths(board, snakeLeft, depth - 1, actualPathLeft)
     else:
-        paths.append(actualPath)
+        #print (actualPath)
+        print("+1")
+        print('\n')
+        increment()
+        if (not actualPath in paths):
+            paths.append(actualPath)
 
 
-
-def numberOfAvailableDifferentPaths(board:list, snake:list, depth:int):
+def numberOfAvailableDifferentPaths(board, snake, depth):
     checking =  checkValidData(board,snake,depth)
     if checking != "Checking succesfull. All data is OK.":
         return checking + "\nRevise the data and retry."
-
+    
+    reset()
     createPaths(board, snake, depth, [])
     pathsNumber = len(paths)
-    print ("The amount of possible paths is: " + pathsNumber)
     return pathsNumber
